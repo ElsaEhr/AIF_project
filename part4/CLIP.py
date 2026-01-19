2ad532e363924864f17ff3d99f5cc3ad5e4f39e0
EJZancanaro <eduzancagarcia@hotmail.com>
2026-01-19 15:45:47 +0100

    Clip.py file added

---
+++ part4/CLIP.py
@@ -0,0 +1,22 @@
+import os
+import clip
+from PIL import Image
+from annoy import AnnoyIndex
+
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+# We are going to load and collect the images which are all in subfolders of a root folder 'root_dir' (choose the path where the images of the project are located)
+
+root_dir = Path.cwd()
+print(root_dir)
+exit()
+image_paths = []
+# collects all the paths of subdirectories
+
+for root, dirs, files in os.walk(root_dir):
+    print("ROOT:", root)
+    print("FILES:", files)
+    for file in files:
+        if file.lower().endswith((".png", ".jpg", ".jpeg")):
+            image_paths.append(os.path.join(root, file))
+
